57bc1a2117ac4e6bc25f84ccb72e9e298e3c8f75
sungry <sbchoi86@gmail.com>
2015-10-01 15:53:55 -0700

    Add polygon modification option, styles

--- challenge2.js
+++
@@ -1,7 +0,0 @@
-// var file = document.getElementById('file').files[0];
-
-function initialize() {
-  var mapCanvas = document.getElementById('map');
-  var map = new google.maps.Map(mapCanvas);
-  google.maps.event.addDomListener(window, 'load', initialize);
-}
